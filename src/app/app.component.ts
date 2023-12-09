b989a4a25dcdcb3c7c851c47c3f52f5108c0a1c2
Jakub <jakublasek41@gmail.com>
2023-12-09 11:55:04 +0100

    Change typography to classes and assign them in inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { TextareaInputComponent } from './ui/molecules/textarea-input/textarea-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TextareaInputComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
