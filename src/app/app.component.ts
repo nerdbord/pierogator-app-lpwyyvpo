d58fcc8e142459609d53dd24aaccfa22d143211e
Jakub <jakublasek41@gmail.com>
2023-12-09 15:43:12 +0100

    Final version of page layout + loader implementation in header section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PageLayoutComponent } from './ui/layouts/page-layout/page-layout.component';
+import { AccordionComponent } from './ui/templates/accordion/accordion.component';
 import { SectionHeaderComponent } from './ui/atoms/section-header/section-header.component';
+import { TextareaInputComponent } from './ui/molecules/textarea-input/textarea-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SectionHeaderComponent],
+  imports: [CommonModule, RouterOutlet, PageLayoutComponent, AccordionComponent, SectionHeaderComponent, TextareaInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/ui/atoms/section-header/section-header.component.ts
+++ src/app/ui/atoms/section-header/section-header.component.ts
@@ -1,15 +1,17 @@
 import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'ui-section-header-atom',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, LoaderComponent],
   templateUrl: './section-header.component.html',
   styleUrl: './section-header.component.scss'
 })
 export class SectionHeaderComponent {
   @Input({required: true}) public sectionName!: string;
+  @Input() public isLoading: boolean = false;
   @Input() public sectionNameOnRight: boolean = false;
   @Input() public buttonText: string = '';
 }
