4f14b0f1ed787d588fc31b25fc5d69f42b34df7f
Jakub <jakublasek41@gmail.com>
2023-12-09 11:14:23 +0100

    Prepare textarea component

---
+++ src/app/ui/atoms/accordion-menu/accordion-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-accordion-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './accordion-menu.component.html',
+  styleUrl: './accordion-menu.component.scss'
+})
+export class AccordionMenuComponent {
+  //TODO
+}
--- src/app/ui/atoms/lock-button/lock-button.component.ts
+++ src/app/ui/atoms/lock-button/lock-button.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, EventEmitter, Output, Signal, WritableSignal, computed, signal } from '@angular/core';
+import { Component, EventEmitter, Input, Output, Signal, WritableSignal, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'ui-lock-button',
@@ -9,6 +9,10 @@ import { Component, EventEmitter, Output, Signal, WritableSignal, computed, sign
   styleUrl: './lock-button.component.scss'
 })
 export class LockButtonComponent {
+  @Input({required: true}) public set lockState(value: boolean) {
+    this.isLocked.set(value);
+  }
+
   @Output() public buttonClick: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   public isLocked: WritableSignal<boolean> = signal(false);
@@ -16,7 +20,6 @@ export class LockButtonComponent {
   public iconPath: Signal<string> = computed(() => (`/assets/icons/${this.isLocked() ? 'locked' : 'unlocked'}.svg`))
 
   public handleButtonClick(): void {
-    this.isLocked.update((currentState) => !currentState);
-    this.buttonClick.emit(this.isLocked());
+    this.buttonClick.emit(!this.isLocked());
   }
 }
