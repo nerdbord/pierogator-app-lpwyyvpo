d3913f5a46866253b6a163dc2ce89beb5a5538a9
Jakub <jakublasek41@gmail.com>
2023-12-10 14:32:46 +0100

    Prepare schema for dumplings request

--- src/app/const/index.ts
+++ src/app/const/index.ts
@@ -1,3 +1,4 @@
 export * from './headers-url.map';
 export * from './image-ai-response.schema';
 export * from './recipe-ai-response.schema';
+export * from './dumpling-description.schema';
--- src/app/pages/dumpling-generator/containers/ingredients/ingredients.component.ts
+++ src/app/pages/dumpling-generator/containers/ingredients/ingredients.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DefaultInputComponent } from '../../../../ui/atoms/default-input/default-input.component';
 import { TextareaInputComponent } from '../../../../ui/molecules/textarea-input/textarea-input.component';
 import { SectionHeaderComponent } from '../../../../ui/molecules/section-header/section-header.component';
+import { OpenAiApiService } from '../../../../services';
+import { AiModelEnum, AiRoleEnum } from '../../../../enums';
+import { ChatCompletionPostBodyInterface } from '../../../../interfaces';
 
 @Component({
   selector: 'dumpling-generator-ingredients',
@@ -11,5 +14,19 @@ import { SectionHeaderComponent } from '../../../../ui/molecules/section-header/
   styleUrl: './ingredients.component.scss'
 })
 export class IngredientsComponent {
+  private _openAiApiService: OpenAiApiService = inject(OpenAiApiService);
+  
+  public generateIngredients(): void {
+    const body: ChatCompletionPostBodyInterface = {
+      model: AiModelEnum.GPT_TURBO,
+      messages: [
+        {
+          role: AiRoleEnum.USER,
+          content: 'Hi, please work as dumpling generator app. Can you please generate information about dumpling'
+        }
+      ]
+    }
 
+    this._openAiApiService.postChatCompletion(body)
+  }
 }
