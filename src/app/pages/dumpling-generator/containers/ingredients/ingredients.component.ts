6ebd5665bbfa60817a933c23cb14e93234422180
Jakub <jakublasek41@gmail.com>
2023-12-10 17:04:08 +0100

    Fix prompt for ingredients

--- src/app/pages/dumpling-generator/containers/ingredients/ingredients.component.ts
+++ src/app/pages/dumpling-generator/containers/ingredients/ingredients.component.ts
@@ -49,11 +49,16 @@ export class IngredientsComponent {
       messages: [
         {
           role: AiRoleEnum.USER,
-          content: `Cześć, pracuj przez chwilę jako aplikacja, która pozwala na generowanie różnych pierogów. 
-          Wygeneruj mi proszę informacje o pierogu który nie istnieje, jednak jest możliwy do stworzenia w realnym świecie. 
-          Może być to zarówno w formie słodkiej jak i słonej.
-          Zależy mi, aby wiadomość była tylko i wyłącznie w formacie podanym wyżej. 
-          Bez żadnego innego tekstu. Oraz niech odpowiedź będzie zawsze w języku polskim`
+           content: `Cześć, pracuj przez chwilę jako kucharz, który opisuje swoje oryginalne pierogi gościom restauracji. 
+            Opisz mi proszę tego pieroga, powiedz krótko o jego cieście, farszu oraz składnikach.
+            Niech ten pieró będzie czymś, czego jeszcze nie jedli.
+            Pieróg może być zarówno w formie słodkiej jak i słonej.
+            Przy informowaniu o skłądnikach nie dodawaj treści w stylu "Składniki potrzebne do ciasta to:". 
+            Wylistuj po przecinku składniki potrzebne do stworzenia ciasta i farszu.
+            Dodatkowo nie dodawaj informacji w stylu "Krótki opis" do swojej odpowiedzi.
+            Zależy mi, aby wiadomość była tylko i wyłącznie w formacie podanym wyżej. 
+            Bez żadnego innego tekstu. Oraz niech odpowiedź będzie zawsze w języku polskim`
+      
         }
       ]
     }
@@ -67,13 +72,12 @@ export class IngredientsComponent {
   public generateImage(): void {
     this.isImageLoading.set(true);
     const body: ImageGenerationPostBodyInterface = {
-      prompt: `
-      Stwórz jedno realistyczne zdjęcie polskiego pieroga. Na zdjęciu ma znaleźc się sam pieróg, nie ma zawierać tekstu, grafik, innych obrazków czy znaków.
-      Zdjęcie wygeneruj na podstawie poniższych informacji dotyczących ciasta, farszu oraz składników z jakich pieróg jest przygotowany.
-      Ciasto:${this.dumplingDescription().dough}.
-      Farsz:${this.dumplingDescription().filling}.
-      Składniki:${this.dumplingDescription().ingredients}.
-     `,
+      prompt: `Proszę o wygenerowanie realistycznego i szczegółowego zdjęcia pieroga.
+        Pieróg powinien znajdować się
+       Pieróg powinien być tradycyjny, polski, z ${this.dumplingDescription().dough}. 
+       Farsz powinien być umieszczony w środku i składać się z ${this.dumplingDescription().filling}. 
+       Pieróg powinien być gotowany, z delikatną, ale jednocześnie sprężystą konsystencją.
+        Pierogi powinny być podane na dużym, płaskim talerzu, w grupie 8-10 sztuk. `,
       n: 1,
       size: `${1024}x${1024}`
     }
